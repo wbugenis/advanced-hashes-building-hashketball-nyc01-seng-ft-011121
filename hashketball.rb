7a394effd89bf3fe0f320803471c4fa73d175217
William Bugenis <wbugenis@gmail.com>
2020-12-16 21:31:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -98,15 +98,15 @@ def game_hash
         slam_dunks: 5 
         }
         
-        {player_name:
-        number: 
-        shoe: 
-        points: 
-        rebounds: 
-        assists: 
-        steals: 
-        blocks: 
-        slam_dunks: 
+        {player_name: "Ben Gordon",
+        number: 8,
+        shoe: 15,
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0 
         }
         
         {player_name:
