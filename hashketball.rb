597919fe14a6ad5cfbed87a90cf53b2bc136e721
William Bugenis <wbugenis@gmail.com>
2020-12-16 21:25:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,26 +76,26 @@ def game_hash
         slam_dunks: 2
         }
         
-        {player_name:
-        number: 
-        shoe: 
-        points: 
-        rebounds: 
-        assists: 
-        steals: 
-        blocks: 
-        slam_dunks: 
+        {player_name: "Bismack Biyombo"
+        number: 0,
+        shoe: 16,
+        points: 12,
+        rebounds: 4, 
+        assists: 7,
+        steals: 22,
+        blocks: 15,
+        slam_dunks: 10 
         }
         
-        {player_name:
-        number: 
-        shoe: 
-        points: 
-        rebounds: 
-        assists: 
-        steals: 
-        blocks: 
-        slam_dunks: 
+        {player_name: "DeSagna Diop"
+        number: 2,
+        shoe: 14,
+        points: 24,
+        rebounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slam_dunks: 5 
         }
         
         {player_name:
