6f0d313ea770e0abe029116aedc3710d464eba17
William Bugenis <wbugenis@gmail.com>
2020-12-16 21:32:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -109,7 +109,7 @@ def game_hash
         slam_dunks: 0 
         },
         
-        {player_name: "Kemba Walker"
+        {player_name: "Kemba Walker",
         number: 33,
         shoe: 15,
         points: 6,
